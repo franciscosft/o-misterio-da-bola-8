7a725a41af7be6c51ee967d579efaabdc7c575ee
Francisco Teixeira <francisco@bry.com.br>
2020-05-08 08:11:30 -0300

    Finalizando projeto - Incluindo ramdomizacao com troca de imagens.

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,52 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
-void main() => runApp(
-      MaterialApp(
-        home: null,
+void main() {
+  runApp(
+    MaterialApp(
+      home: BallPage(),
+    ),
+  );
+}
+
+class BallPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.blue,
+      appBar: AppBar(
+        backgroundColor: Colors.blue.shade900,
+        title: Text('Pergunte-me qualquer coisa'),
+      ),
+      body: Ball(),
+    );
+  }
+}
+
+class Ball extends StatefulWidget {
+  @override
+  _BallState createState() => _BallState();
+}
+
+class _BallState extends State<Ball> {
+  int number = 1;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Center(
+        child: FlatButton(
+          child: Image.asset('images/ball$number.png'),
+          onPressed: () {
+            setState(() {
+              number = Random().nextInt(5) + 1;
+              print('Deixe a mágica acontecer');
+              print(number);
+            });
+          },
+        ),
       ),
     );
+  }
+}
